e272a077c9db4a197372ed72b429286d957b814f
milesrl <soccermiles1@gmail.com>
2017-10-29 10:02:10 -0600

    a new command, uniform readable declarations

--- main.rs
+++ main.rs
@@ -64,78 +64,107 @@ impl Calculator {
 
         // binary floating point operations
         calc.ops.insert(String::from("-"),
-            make_binop(String::from("Subtract"), Box::new(|a, b| a - b), String::from("push(pop - pop)")));
+            make_binop(String::from("Subtract"),
+            Box::new(|a, b| a - b),
+            String::from("push(pop - pop)")));
         calc.ops.insert(String::from("+"),
-            make_binop(String::from("Add"), Box::new(|a, b| a + b), String::from("push(pop + pop)")));
+            make_binop(String::from("Add"),
+            Box::new(|a, b| a + b),
+            String::from("push(pop + pop)")));
         calc.ops.insert(String::from("*"),
-            make_binop(String::from("Multiply"), Box::new(|a, b| a * b), String::from("push(pop * pop)")));
+            make_binop(String::from("Multiply"),
+            Box::new(|a, b| a * b),
+            String::from("push(pop * pop)")));
         calc.ops.insert(String::from("/"),
-            make_binop(String::from("Divide"), Box::new(|a, b| a / b), String::from("push(pop / pop)")));
+            make_binop(String::from("Divide"),
+            Box::new(|a, b| a / b),
+            String::from("push(pop / pop)")));
         calc.ops.insert(String::from("^"),
-            make_binop(String::from("Exponentiate"), Box::new(|a, b| a.powf(b)), String::from("push(pop ^ pop)")));
-        calc.ops.insert(String::from("e"),
-            make_binop(String::from("Times Ten To The ..."),
+            make_binop(String::from("Exponentiate"),
+            Box::new(|a, b| a.powf(b)),
+            String::from("push(pop ^ pop)")));
+        calc.ops.insert(String::from("*e^"),
+            make_binop(String::from("Times Ten to the ..."),
             Box::new(|a, b| a * (10.0 as f64).powf(b)),
             String::from("push(pop * (10 ^ pop))")));
+        calc.ops.insert(String::from("/e^"),
+            make_binop(String::from("Divided by Ten to the ..."),
+            Box::new(|a, b| a / (10.0 as f64).powf(b)),
+            String::from("push(pop / (10 ^ pop))")));
         calc.ops.insert(String::from("log"),
-            make_binop(String::from("Logarithm"), Box::new(|a, b| a.log(b)), String::from("push(log_pop(pop)")));
+            make_binop(String::from("Logarithm"),
+            Box::new(|a, b| a.log(b)),
+            String::from("push(log_pop(pop)")));
         calc.ops.insert(String::from(">"),
-            make_binop(String::from("Greater Than"), Box::new(|a, b| bool_to_f64(a > b)), String::from("push(pop > pop)")));
+            make_binop(String::from("Greater Than"),
+            Box::new(|a, b| bool_to_f64(a > b)),
+            String::from("push(pop > pop)")));
         calc.ops.insert(String::from("<"),
-            make_binop(String::from("Less Than"), Box::new(|a, b| bool_to_f64(a < b)), String::from("push(pop < pop)")));
+            make_binop(String::from("Less Than"),
+            Box::new(|a, b| bool_to_f64(a < b)),
+            String::from("push(pop < pop)")));
         calc.ops.insert(String::from("=="),
-            make_binop(String::from("Equal?"), Box::new(|a, b| bool_to_f64(a == b)), String::from("push(pop == pop)")));
+            make_binop(String::from("Equal?"),
+            Box::new(|a, b| bool_to_f64(a == b)),
+            String::from("push(pop == pop)")));
 
         // binary logical operations
-        calc.ops.insert(String::from("nand"), make_binop(
-            String::from("Nand"),
+        calc.ops.insert(String::from("nand"),
+            make_binop(String::from("Nand"),
             Box::new(|a, b| bool_to_f64(
                 !(f64_to_bool(a) && f64_to_bool(b))
             )),
-            String::from("push(not(pop and pop))")
-        ));
-        calc.ops.insert(String::from("and"), make_binop(
-            String::from("And"),
+            String::from("push(not(pop and pop))")));
+        calc.ops.insert(String::from("and"),
+            make_binop(String::from("And"),
             Box::new(|a, b| bool_to_f64(
                 f64_to_bool(a) && f64_to_bool(b)
             )),
-            String::from("push(pop and pop)")
-        ));
-        calc.ops.insert(String::from("or"), make_binop(
-            String::from("Or"),
+            String::from("push(pop and pop)")));
+        calc.ops.insert(String::from("or"),
+            make_binop(String::from("Or"),
             Box::new(|a, b| bool_to_f64(
                 f64_to_bool(a) || f64_to_bool(b)
             )),
-            String::from("push(pop or pop)")
-        ));
-        calc.ops.insert(String::from("xor"), make_binop(
-            String::from("Xor"),
+            String::from("push(pop or pop)")));
+        calc.ops.insert(String::from("xor"),
+            make_binop(String::from("Xor"),
             Box::new(|a, b| bool_to_f64(
                 f64_to_bool(a) ^ f64_to_bool(b)
             )),
-            String::from("push(pop xor pop)")
-        ));
+            String::from("push(pop xor pop)")));
         
         // unary floating point operations
         calc.ops.insert(String::from("ln"),
-            make_unop(String::from("Natural Logarithm"), Box::new(|x| x.ln()), String::from("push(ln(pop))")));
+            make_unop(String::from("Natural Logarithm"),
+            Box::new(|x| x.ln()),
+            String::from("push(ln(pop))")));
         calc.ops.insert(String::from("lg"),
-            make_unop(String::from("Logarithm Base 2"), Box::new(|x| x.log2()), String::from("push(lg(pop))")));
+            make_unop(String::from("Logarithm Base 2"),
+            Box::new(|x| x.log2()),
+            String::from("push(lg(pop))")));
         calc.ops.insert(String::from("inf?"),
-            make_unop(String::from("Infinite?"), Box::new(|x| bool_to_f64(x.is_infinite())), String::from("push(whether pop is infinite)")));
+            make_unop(String::from("Infinite?"),
+            Box::new(|x| bool_to_f64(x.is_infinite())),
+            String::from("push(whether pop is infinite)")));
         calc.ops.insert(String::from("nan?"),
-            make_unop(String::from("Not A Number?"), Box::new(|x| bool_to_f64(x.is_nan())), String::from("push(whether pop is NaN)")));
+            make_unop(String::from("Not A Number?"),
+            Box::new(|x| bool_to_f64(x.is_nan())),
+            String::from("push(whether pop is NaN)")));
         calc.ops.insert(String::from("sign"),
-            make_unop(String::from("Sign"), Box::new(|x| x.signum()), String::from("push(sign of pop)")));
+            make_unop(String::from("Sign"),
+            Box::new(|x| x.signum()),
+            String::from("push(sign of pop)")));
         calc.ops.insert(String::from("fin?"),
-            make_unop(String::from("Finite?"), Box::new(|x| bool_to_f64(x.is_finite())), String::from("push(whether pip is finite)")));
+            make_unop(String::from("Finite?"),
+            Box::new(|x| bool_to_f64(x.is_finite())),
+            String::from("push(whether pip is finite)")));
 
         // unary logical operations
         calc.ops.insert(String::from("not"), make_unop(
             String::from("Not"),
             Box::new(|x| bool_to_f64(!f64_to_bool(x))),
-            String::from("push(not(pop))")
-        ));
+            String::from("push(not(pop))")));
 
         // stack manipulation and printing
         calc.ops.insert(String::from("print"),
